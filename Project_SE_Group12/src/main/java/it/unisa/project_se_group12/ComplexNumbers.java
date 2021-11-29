31c6018d137b6dcfeeea6837ea5dc2f33ee0a348
Alessandra <A.DELUCA84@studenti.unisa.it>
2021-11-29 16:46:17 +0100

    square root commit

--- Project_SE_Group12/src/main/java/it/unisa/project_se_group12/ComplexNumbers.java
+++ Project_SE_Group12/src/main/java/it/unisa/project_se_group12/ComplexNumbers.java
@@ -64,9 +64,15 @@ public class ComplexNumbers {
     }
     
    public ComplexNumbers squareRootComplex(ComplexNumbers n1){
-        double r=Math.sqrt(mod(n1));
-        double theta=Math.atan2(n1.getImaginary(),n1.getReal())/2;
-        return new ComplexNumbers(r*Math.cos(theta),r*Math.sin(theta));
+       double r=Math.sqrt(mod(n1));
+        if(n1.getReal()<0 && n1.getImaginary()==0){
+            return new ComplexNumbers(0,r);
+        }else{
+            
+            double theta=Math.atan2(n1.getImaginary(),n1.getReal())/2;
+            return new ComplexNumbers(r*Math.cos(theta),r*Math.sin(theta));
+        }
+        
     }
     
     public ComplexNumbers conjugate(ComplexNumbers n1){
