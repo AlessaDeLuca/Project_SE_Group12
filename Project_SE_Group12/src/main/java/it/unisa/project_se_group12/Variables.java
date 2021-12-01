ab5f64d5c6f864868a2b00f7972987aabe55cbb8
Alessandra <A.DELUCA84@studenti.unisa.it>
2021-12-01 18:22:39 +0100

    variables commit

---
+++ Project_SE_Group12/src/main/java/it/unisa/project_se_group12/Variables.java
@@ -0,0 +1,47 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.project_se_group12;
+
+import java.util.HashMap;
+import java.util.Stack;
+
+/**
+ *
+ * @author PC MSI
+ */
+public class Variables {
+    HashMap<String, ComplexNumbers> map = new HashMap<>();
+    String [] key = {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"};
+
+    public Variables() {
+        for (int i=0; i<key.length-1; i++){
+            this.map.put(this.key[i], null);
+        } 
+    }
+    
+    public HashMap<String, ComplexNumbers> getMap() {
+        return map;
+    }
+    
+    public void pushIntoVariable(String varKey, StackManage stack){
+        this.map.replace(varKey, stack.getStack().lastElement());
+    }
+
+    public void pushIntoStack(String varKey, StackManage stack){
+        stack.getStack().push(map.get(varKey));
+    }
+    
+    public void addWithStack(String varKey, StackManage stack){
+        ComplexNumbers stackNum = stack.getStack().lastElement();
+        ComplexNumbers mapNum = this.map.get(varKey);
+        this.map.replace(varKey, new ComplexOperations().add(stackNum, mapNum));
+    }
+    
+    public void subWithStack(String varKey, StackManage stack){
+        ComplexNumbers stackNum = stack.getStack().lastElement();
+        ComplexNumbers mapNum = this.map.get(varKey);
+        this.map.replace(varKey, new ComplexOperations().subtract(mapNum, stackNum));
+    }
+}
