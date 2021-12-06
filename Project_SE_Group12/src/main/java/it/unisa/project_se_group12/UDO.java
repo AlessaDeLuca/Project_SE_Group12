328681253b331a398c60fceea918b48b6ffa4267
Alessandra <A.DELUCA84@studenti.unisa.it>
2021-12-06 11:56:58 +0100

    UDO commit

---
+++ Project_SE_Group12/src/main/java/it/unisa/project_se_group12/UDO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.project_se_group12;
+
+/**
+ *
+ * @author PC MSI
+ */
+public class UDO {
+    
+}
